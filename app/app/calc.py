0f9f44ead2b5652b42566d039f45fd551f892b77
WuKai12 <wusxocompany@gmail.com>
2025-05-21 15:04:33 +0700

    Add spycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator functions
+"""
+def add(x, y):
+    """Add x and y and return results."""
+    return x + y
+
+def subtract(x,y):
+    """Subtract x and y and return return results."""
+    return 0
